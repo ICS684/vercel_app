43604343a4fc5e1f478656435439896fa421962d
jlo2224 <jaredlo@hawaii.edu>
2025-11-20 11:16:13 -1000

    added minimal plotly plot (no data loaded)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import { Col, Container, Row } from 'react-bootstrap';
 import USMap from '../components/USMap';
+import BubbleMap from '../components/BubbleMap';
+
 
 /** The Home page. */
 const Home = () => (
@@ -9,7 +11,8 @@ const Home = () => (
         <Col xs={12} className="d-flex flex-column align-items-center mt-5">
           <h1 className="mb-4">ZHVI Visualization</h1>
           <div style={{ width: '100%', maxWidth: 900 }}>
-            <USMap />
+            {/*<USMap />*/}
+            <BubbleMap />
           </div>
         </Col>
       </Row>
