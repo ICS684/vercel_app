00b8dbfe0ffdb4c6fccbccad24c0c3940f46a73b
elijah-saloma <esaloma@hawaii.edu>
2025-12-11 09:50:30 -1000

    explanation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,6 +37,21 @@ const Home = () => (
               fontSize: 14,
             }}
           >
+            Using ZHVI Single Family Home Time Series (ZIP code) as a metric, we can generalize price patterns and
+            highlights regional trends in housing values. Through this visualization, the geographical wealth and
+            housing developmental patterns of the years throughout 2000 - 2025 are visible. The bubbles scale from
+            dark blue to yellow and small to large, based on lower average price and higher average price respectively.
+            Each bubble represents a region of houses that have some ZHVI value, and upon clicking one, reveals the ZHVI
+            trends for that location from 2000 - 2025.
+          </p>
+          <p
+            style={{
+              marginTop: -12,
+              marginBottom: 24,
+              color: '#9ca3af',
+              fontSize: 14,
+            }}
+          >
             Explore average single-family home values across the United States. Zoom in to
             reveal finer-grained bubbles.
           </p>
